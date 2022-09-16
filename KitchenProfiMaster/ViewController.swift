cc35528566df35471adfc948fc35cb37a2866d8a
LxEugenxG <eugen.ganske87@gmail.com>
2022-09-16 13:31:59 +0200

    Initial Commit

---
+++ KitchenProfiMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KitchenProfiMaster
+//
+//  Created by Eugen Ganske on 16.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
